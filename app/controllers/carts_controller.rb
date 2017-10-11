efd745db8e9659c36ebf3f1d3fdf8e41897e3bcd
Ali Abji <aliabji@gmail.com>
2017-10-11 23:09:14 +0000

    removed add to cart button from sold out products

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,7 +5,7 @@ class CartsController < ApplicationController
 
   def add_item
     product_id = params[:product_id].to_s
-
+    
     item = cart[product_id] || { "quantity" => 0 }
     item["quantity"] += 1
     cart[product_id] = item

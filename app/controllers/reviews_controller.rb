88a5b761626b7ebb90ab8331f9025a9e10ab9675
Ali Abji <aliabji@gmail.com>
2017-10-13 15:39:21 +0000

    new reviews submitting and displaying on product page

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,24 @@
+class ReviewsController < ApplicationController
+
+  def create
+
+    
+    @product = Product.find params[:product_id]
+    @review = @product.new_review(review_params, current_user)
+    new_review = @review
+    product_show = @product
+
+    if @review.save
+      redirect_to product_show, notice: 'Your review has been submitted. Thank you.'
+    else
+      redirect_to product_show
+    end
+
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :rating, :product_id, :user_id)
+  end
+end
